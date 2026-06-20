c7f24953d9a0a145aed960c360bb973072ef38c0
GSHugo <vto.garciasanchez98@gmail.com>
2026-06-19 19:15:31 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG_APP = {
+    // URL oficial de tu Google Apps Script integrada con éxito
+    URL_API_GOOGLE: "https://script.google.com/macros/s/AKfycbxRmmKDcge_ohWv6Djuo3kmyy6ak7VD3okCAEIZ3BvIHQkAmxE5De82N-gSCpwj0PjvZA/exec"
+};
