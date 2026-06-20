58ecda86f4c34ad66fe0cce1d7756ae0bc54b4ad
GSHugo <vto.garciasanchez98@gmail.com>
2026-06-19 20:10:27 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG_APP = {
     // Tu enlace absoluto del Web App de Google Sheets
-    URL_API_GOOGLE: "https://script.google.com/macros/s/AKfycbwMnKlrV4HCAZMVQOlnKvMENI9LdZ2bVUUlTvKgW2u8NHQPusfMQP3chsPwBRY2BgcKlg/exec"
+    URL_API_GOOGLE: "https://script.google.com/macros/s/AKfycbzhD-quaf0gfHNmryLsJxmDK5xcXeDubhg-8InqFqydlouz1ZqwF2pCjTn6m5fTtQmJdg/exec"
 };
