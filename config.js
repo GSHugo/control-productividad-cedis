998083b5eef0c038b7d8a8c257d0757bf9ff5b0d
GSHugo <vto.garciasanchez98@gmail.com>
2026-06-19 19:29:58 -0600

    Update config.js
    Updated comment to clarify the purpose of the URL.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG_APP = {
-    // URL oficial de tu Google Apps Script integrada con éxito
+    // Tu enlace absoluto del Web App de Google Sheets
     URL_API_GOOGLE: "https://script.google.com/macros/s/AKfycbxRmmKDcge_ohWv6Djuo3kmyy6ak7VD3okCAEIZ3BvIHQkAmxE5De82N-gSCpwj0PjvZA/exec"
 };
